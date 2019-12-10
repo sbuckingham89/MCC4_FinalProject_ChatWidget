64e4e40b7f5ddf06c95d1626a26df12c9a0b9622
Sarah Buckingham <sarah.rose.buckingham89@gmail.com>
2019-12-10 00:29:16 +0000

    adding widget

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    instanceLocator: 'v1:us1:02b78c87-5aa4-41a2-9ff1-36444172cd1f',
+    key: '3e6effb4-7aab-45a7-9ca9-5e088a1d72a6:O5Wan6ZDyr5ZatlHXW6Bg3H/pplg6H+cZl6WCgdPYXQ='
+}
